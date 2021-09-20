4dd4303a12430e7b6995d3879ce983c1c3ffd7aa
dkelly255 <dkelly25@hotmail.co.uk>
2021-09-20 08:20:28 +0000

    Add initial rev of division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,14 +42,16 @@ function runGame(gameType) {
             displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
             displaySubtractQuestion(num1, num2);
-    }   else {
+    } else if (gameType === "division") {
+            displayDivisionQuestion(num1, num2);
+    }  else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
 }
 
 /**
- * Checks the answer agaist the first element in
+ * Checks the answer against the first element in
  * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
@@ -85,6 +87,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -133,4 +137,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "X";
+}
+
+function displayDivisionQuestion (operand1, operand2) {
+ 
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
